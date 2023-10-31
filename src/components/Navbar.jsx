2e8f31af5679e5fdf1e852782afbb422b37d00d6
Paul Airuehia <osasuair@gmail.com>
2023-10-31 01:37:42 -0400

    updated icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaIcons } from 'react-icons/fa'
+import favicon from '../images/favicon.png'
 
 const Navbar = () => {
   const startAgainClick = () => {
@@ -9,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center w-full h-16 px-10 text-black bg-white fixed top-0 z-50'>
         <div className='flex flex-row items-center'>
-            <FaIcons size={25} color='black'></FaIcons>
+            <img src={favicon} className='w-10 h-10 rounded-lg'></img>
             <h1 className='pl-2 text-3xl'>Ideas2Audio</h1>
         </div>
         <div className='flex flex-row font-bold'>
